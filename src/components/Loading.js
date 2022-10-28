e7ff10f76800ebfba695b4989d76e35d6d952a96
j2noo <wlsdndml213@naver.com>
2022-10-28 12:48:13 +0900

    Loading.js

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,4 +1,19 @@
+import "./Loading.css";
+
 function Loading() {
-  return <h1>Loading..</h1>;
+  const tip = [
+    "이 문구는 새로고침 할때마다 바뀌어요",
+    "공부는 꾸준히 하자구요",
+    "전 디자인 감각이 없는거 같아요",
+    "css를 잘다루고 싶어요",
+    "제 최애영화는 탑건이에요",
+  ];
+  const rand = Math.floor(Math.random() * tip.length);
+  return (
+    <div id="loadingDiv">
+      <p id="loadingP">Loading..</p>
+      <p id="tipP">{`※ ${tip[rand]}`}</p>
+    </div>
+  );
 }
 export default Loading;
